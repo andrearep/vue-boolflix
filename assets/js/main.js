12884ded7aed5b1f88a2e3e033a9509763026037
andrearep <andrea.repe@gmail.com>
2021-06-08 14:23:32 +0200

    creata funzione per cercare i film

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,26 @@
+function replaceSpaceWPlus(string) {
+    return string.replace(' ', '+')
+}
+
 const app = new Vue({
+
     el: "#root",
     data: {
+        filmName: null,
+        filmNamePlus: null
 
     },
     methods: {
-
+        searchFilm: function serchFilm() {
+            this.filmNamePlus = replaceSpaceWPlus(this.filmName);
+            axios
+                .get(`https://api.themoviedb.org/3/search/movie?api_key=e99307154c6dfb0b4750f6603256716d&query=${this.filmNamePlus}`)
+                .then(resp => {
+                    console.log(resp.data.results);
+                })
+        }
     },
 
     mounted() {
-        axios
-            .get("https://api.themoviedb.org/3/movie/550?api_key=8cdf7854b5e8d81f642d19f2e6ddd5d4")
-            .then(resp => {
-                console.log(resp.data);
-            })
     }
 })
