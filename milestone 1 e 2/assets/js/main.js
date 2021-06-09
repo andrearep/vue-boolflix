02a40014145ee465f0ed8d6e1af1ab9d935fe715
andrearep <andrea.repe@gmail.com>
2021-06-09 11:02:05 +0200

    racchiuso url e liste in un oggetto e modificate le funz

--- milestone 1 e 2/assets/js/main.js
+++ milestone 1 e 2/assets/js/main.js
@@ -1,57 +1,68 @@
-function replaceSpaceWPlus(string) {
-    return string.replace(' ', '+')
-}
+/**
+ * ritorna l'oggetto dat con una chiave flag contenente il link all'immagine della bandiera
+ * @param {object} object  deve contenere un elemento original_language per poter essere valida
+ * @returns {object} object l'oggettto iniziale con l'elemento flag
+ */
 function addFlag(object) {
     for (let i = 0; i < object.length; i++) {
-        if (object[i].original_language != 'en') {
-            object[i].flag = `https://flagcdn.com/${object[i].original_language}.svg`
+
+        if (object[i].original_language == 'ja') {
+            object[i].flag = `https://flagcdn.com/jp.svg`
         } else if (object[i].original_language == 'en') {
             object[i].flag = `https://flagcdn.com/gb.svg`
         } else {
-            object[i].flag = `https://upload.wikimedia.org/wikipedia/commons/thumb/e/ef/International_Flag_of_Planet_Earth.svg/220px-International_Flag_of_Planet_Earth.svg.png`
+            object[i].flag = `https://flagcdn.com/${object[i].original_language}.svg`
         }
+        console.log(object[i].flag);
     }
     return object;
 }
+
 
 const app = new Vue({
 
     el: "#root",
     data: {
         filmSearch: null,
-        filmSearchPlus: null,
-        films: [],
-        series: []
-
+        filmSerie: [
+            /* film */
+            {
+                name: "film",
+                movieList: [],
+                url: "https://api.themoviedb.org/3/search/movie?api_key=e99307154c6dfb0b4750f6603256716d&query="
+            },
+            /* serie tv */
+            {
+                name: "Serie TV",
+                movieList: [],
+                url: "https://api.themoviedb.org/3/search/tv?api_key=e99307154c6dfb0b4750f6603256716d&language=it_IT&query="
+            }
+        ]
     },
     methods: {
 
-        searchFilm: function searchFilm() {
-            this.filmSearchPlus = replaceSpaceWPlus(this.filmSearch);
-            axios
-                .get(`https://api.themoviedb.org/3/search/movie?api_key=e99307154c6dfb0b4750f6603256716d&query=${this.filmSearchPlus}`)
-                .then(resp => {
-                    this.films = resp.data.results
-                    this.films = addFlag(this.films)
-                })
-        },
+        search: function search() {
+            this.filmSerie.forEach((element) => {
 
-        searchSerie: function searchSerie() {
-            this.filmSearchPlus = replaceSpaceWPlus(this.filmSearch);
-            axios
-                .get(`https://api.themoviedb.org/3/search/tv?api_key=e99307154c6dfb0b4750f6603256716d&language=it_IT&query=${this.filmSearchPlus}`)
-                .then(resp => {
-                    this.series = resp.data.results
-                    this.series = addFlag(this.series)
-                })
+                axios
+                    .get(`${element.url}${this.filmSearch}`)
+                    .then(resp => {
+                        element.movieList = (resp.data.results)
+                        element.movieList.flag = addFlag(element.movieList)
+                        console.log(`${element.url}${this.filmSearch}`);
+                    })
+                    .catch(e => {
+                        console.error(e);
+                    })
+            })
         }
     },
 
     mounted() {
         document.addEventListener('keyup', (e) => {
             if (e.key === 'Enter') {
-                this.searchFilm();
-                this.searchSerie()
+                this.search();
+
             }
         })
     }
